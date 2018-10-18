947a557b6cbfeb26fa64ade66f5ca56433db64c3
wangchao <455287693@163.com>
2018-10-18 17:14:14 +0800

    Initial MyFirstRepository

---
+++ Sources/ClassA.h
@@ -0,0 +1,15 @@
+//
+//  ClassA.h
+//  MyTest
+//
+//  Created by 王超 on 2018/10/18.
+//  Copyright © 2018年 王超. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ClassA : NSObject
+
+- (void)method_A;
+
+@end
---
+++ Sources/ClassA.m
@@ -0,0 +1,17 @@
+//
+//  ClassA.m
+//  MyTest
+//
+//  Created by 王超 on 2018/10/18.
+//  Copyright © 2018年 王超. All rights reserved.
+//
+
+#import "ClassA.h"
+
+@implementation ClassA
+
+- (void)method_A {
+    NSLog(@"---- I am Method A");
+}
+
+@end
